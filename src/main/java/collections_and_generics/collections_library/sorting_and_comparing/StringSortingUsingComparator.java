f381f02c7f83c2200e1c6622e2ac2ca5bce1923f
imcvk <chaitanyakulkarni789@gmail.com>
2025-06-24 21:18:16 +0530

    core java repo update- 24-06-2025

---
+++ src/main/java/collections_and_generics/collections_library/sorting_and_comparing/StringSortingUsingComparator.java
@@ -0,0 +1,40 @@
+package collections_and_generics.collections_library.sorting_and_comparing;
+
+import collections_and_generics.collections_library.CollectionUtility;
+
+import java.util.*;
+
+class StringListInAscendingOrder implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2.length();
+    }
+}
+
+class StringListInDescendingOrder implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.length() - o1.length();
+    }
+}
+
+public class StringSortingUsingComparator {
+    public static void main(String[] args) {
+        List<String> list = CollectionUtility.getNameList(ArrayList::new);
+        System.out.println(list);
+
+        System.out.println("List of names in Ascending order based on names:");
+        list.sort(new StringListInAscendingOrder());
+        System.out.println(list);
+
+        System.out.println("Reversing the list of names using comparator:");
+        list.sort(new StringListInDescendingOrder());
+        System.out.println(list);
+        ArrayList<String> strings = CollectionUtility.getNameList(ArrayList::new);
+        System.out.println("List before sorting using default comparator:" + strings);
+        Collections.sort(strings);
+        System.out.println("String sorting using default comparator:"+strings);
+        Collections.reverse(strings);
+        System.out.println("Reverse sorting of list of Strings using default comparator:"+strings);
+    }
+}
